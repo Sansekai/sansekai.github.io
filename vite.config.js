635b2a381ade7b03e752fc295e1e5228e2de05c8
Sansekai <m.yusril925@gmail.com>
2023-07-03 22:32:28 +0800

    Update

--- vite.config.js
+++ vite.config.js
@@ -21,9 +21,9 @@ export default defineConfig({
       },
       includeAssets: ['logo.png'],
       manifest: {
-        name: 'Portfolio',
-        short_name: 'Portfolio',
-        description: 'Personal Portfolio',
+        name: 'Portofolio',
+        short_name: 'Portofolio',
+        description: 'Personal Portofolio',
         icons: [
           {
             src: 'logo.png',
