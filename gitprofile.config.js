6adf7d6d201815a89e1f543964beda8d0777e82d
Sansekai <m.yusril925@gmail.com>
2023-07-03 22:28:10 +0800

    Update

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -30,7 +30,7 @@ const config = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      '', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'Still Learning',
